9df0354836b9c5c94589168f9335fd4a5a791b9d
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2022-05-28 10:41:45 +0900

    ボタンのリスナー追加

--- app/src/main/java/com/example/sortandfilter/FilterDialogFragment.kt
+++ app/src/main/java/com/example/sortandfilter/FilterDialogFragment.kt
@@ -21,42 +21,24 @@ class FilterDialogFragment : DialogFragment() {
     private val viewModel: FilterDialogViewModel by viewModels()
     private lateinit var binding: FragmentFilterDialogBinding
 
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//        binding = FragmentFilterDialogBinding.inflate(inflater, container, false)
-//        binding.viewModel = viewModel
-//        binding.lifecycleOwner = viewLifecycleOwner
-//        return binding.root
-//    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.viewModel = viewModel
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        //return super.onCreateDialog(savedInstanceState)
-
         val dialog = Dialog(requireContext())
-        // ダイアログの背景を透過にする
-        //dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
-        // Bundleに渡した値を取り出す
-//        arguments?.let {
-//            title = it.getString("TitleKey", "")
-//            message = it.getString("MesasgeKey", "")
-//        }
-//        val binding = LayoutCustomDialogBinding.inflate(requireActivity().layoutInflater)
-//        binding.title.text = title
-//        binding.message.text = message
-//        binding.positiveButton.text = "OK"
-//        binding.negativeButton.text = "キャンセル"
-
         binding = FragmentFilterDialogBinding.inflate(requireActivity().layoutInflater)
         binding.viewModel = viewModel
-//        binding.lifecycleOwner = viewLifecycleOwner
+        binding.lifecycleOwner = this
+
+        binding.btnOk.setOnClickListener {
+            dismiss()
+        }
+
+        binding.btnCancel.setOnClickListener {
+            dismiss()
+        }
+
         dialog.setContentView(binding.root)
         return dialog
     }
--- app/src/main/java/com/example/sortandfilter/SortDialogFragment.kt
+++ app/src/main/java/com/example/sortandfilter/SortDialogFragment.kt
@@ -2,9 +2,7 @@ package com.example.sortandfilter
 
 import android.app.Dialog
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
@@ -23,42 +21,20 @@ class SortDialogFragment : DialogFragment() {
     private val viewModel: SortDialogViewModel by viewModels()
     private lateinit var binding: FragmentSortDialogBinding
 
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//        binding = FragmentSortDialogBinding.inflate(inflater, container, false)
-//        binding.viewModel = viewModel
-//        binding.lifecycleOwner = viewLifecycleOwner
-//        return binding.root
-//    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.viewModel = viewModel
-    }
-
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        //return super.onCreateDialog(savedInstanceState)
-
         val dialog = Dialog(requireContext())
-        // ダイアログの背景を透過にする
-        //dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
-        // Bundleに渡した値を取り出す
-//        arguments?.let {
-//            title = it.getString("TitleKey", "")
-//            message = it.getString("MesasgeKey", "")
-//        }
-//        val binding = LayoutCustomDialogBinding.inflate(requireActivity().layoutInflater)
-//        binding.title.text = title
-//        binding.message.text = message
-//        binding.positiveButton.text = "OK"
-//        binding.negativeButton.text = "キャンセル"
-
         binding = FragmentSortDialogBinding.inflate(requireActivity().layoutInflater)
         binding.viewModel = viewModel
-//        binding.lifecycleOwner = viewLifecycleOwner
+        binding.lifecycleOwner = this
+
+        binding.btnOk.setOnClickListener {
+            dismiss()
+        }
+
+        binding.btnCancel.setOnClickListener {
+            dismiss()
+        }
+
         dialog.setContentView(binding.root)
         return dialog
     }
